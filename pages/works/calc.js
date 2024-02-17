090dd737f903dd74b1023b53a531a5d0397946b3
Esterben <gabrielestebanv@gmail.com>
2024-02-17 14:36:51 -0500

    Fix[link] Calculadora repositorio

--- pages/works/calc.js
+++ pages/works/calc.js
@@ -23,8 +23,8 @@ const Work = () => (
       <List ml={4} my={4}>
         <ListItem>
           <Meta>Website</Meta>
-          <Link href="#">
-            PONER LINK AQUI <ExternalLinkIcon mx="2px" />
+          <Link href="https://github.com/Esterben/Proyecto-Calculadora">
+            Repositorio <ExternalLinkIcon mx="2px" />
           </Link>
         </ListItem>
         <ListItem>
